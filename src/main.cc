dd7734d5fd62dc8e3cf022a061f25e9b955bb6bd
noah.nyounae <noah.nyounae@epita.fr>
2025-05-28 00:48:31 +0000

    v1.5 sha1024 main args update

--- src/main.cc
+++ src/main.cc
@@ -2,11 +2,15 @@
 #include <string>
 #include "sha1024.hxx"
 
-int main() {
+int main(int argc, char* argv[]) {
     std::string input;
-    std::cout << "Enter a string to hash: ";
-    std::getline(std::cin, input);
+    if (argc > 1) {
+        input = argv[1];
+    } else {
+        std::cout << "Enter a string to hash: ";
+        std::getline(std::cin, input);
+    }
     std::string hash = sha1024(input);
-    std::cout << "\nSHA-1024:\n" << hash << std::endl;
+    std::cout << "SHA-1024(" << input << "):\n" << hash << std::endl;
     return 0;
 }
