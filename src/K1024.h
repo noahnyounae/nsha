695fd383fa5c953b44eb7964c2ef75034404d58e
noah.nyounae <noah.nyounae@epita.fr>
2025-05-28 00:21:38 +0000

    v1.2

--- src/K1024.h
+++
@@ -1,9 +0,0 @@
-#ifndef K1024_HXX
-#define K1024_HXX
-
-#include <cstdint>
-
-// Extern declaration for SHA-1024 round constants
-extern const uint64_t K[128];
-
-#endif // K1024_HXX
--- src/initState1024.h
+++
@@ -1,9 +0,0 @@
-#ifndef INITSTATE1024_HXX
-#define INITSTATE1024_HXX
-
-#include <cstdint>
-
-// Initialize the SHA-1024 state array H[16]
-void initState1024(uint64_t H[16]);
-
-#endif // INITSTATE1024_HXX
